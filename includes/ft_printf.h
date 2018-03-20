575317cd0a0ef1b4bbd134c5e937de548368f294
Rafael Zarate <rafazrte@gmail.com>
2018-03-20 11:43:09 -0700

    Finished norming this sss out

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rzarate <rzarate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 21:41:23 by rzarate           #+#    #+#             */
-/*   Updated: 2018/03/19 22:36:49 by rzarate          ###   ########.fr       */
+/*   Updated: 2018/03/20 11:24:46 by rzarate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,18 +34,18 @@ typedef	union	u_mst
 	ssize_t						sst;
 	size_t						st;
 	char						*s;
-    wchar_t						wct;
+	wchar_t						wct;
 	wchar_t						*wcts;
 	void						*p;
 }				t_uni;
 
 typedef	struct	s_mst
 {
-	int 	*hash;
-	int 	*zero;
-	int 	*minus;
-	int 	*plus;
-	int 	*space;
+	int		*hash;
+	int		*zero;
+	int		*minus;
+	int		*plus;
+	int		*space;
 	int		*mfw;
 	int		*dot;
 	int		*precision;
@@ -54,17 +54,23 @@ typedef	struct	s_mst
 	int		*n_chars;
 }				t_mst;
 
-/* MAIN FUNCTIONS */
+/*
+** MAIN FUNCTIONS
+*/
 int				ft_printf(const char *format, ...);
 
-/* SCANNING FUNCTIONS */
+/*
+** SCANNING FUNCTIONS
+*/
 int				check_flags(char *fmt, int x, t_mst *args, int i2);
 int				check_mfw(char *fmt, int x, t_mst *args, int i2);
 int				check_precision(char *fmt, int x, t_mst *args, int i2);
 int				check_mod(char *fmt, int x, t_mst *args, int i2);
 char			check_ids(char c);
 
-/* HANDLING */
+/*
+** HANDLING
+*/
 int				handle_i(t_mst *args, int i, t_uni *d_type);
 int				handle_o(t_mst *args, int i, t_uni *d_type);
 int				handle_u(t_mst *args, int i, t_uni *d_type);
@@ -73,7 +79,6 @@ int				handle_c(t_mst *args, int i, t_uni *d_type);
 int				handle_s(t_mst *args, int i, t_uni *d_type);
 int				handle_p(t_uni *d_type);
 int				handle_per(t_mst *args, int i);
-//int				handle_invalid(t_mst *args, int i);
 
 void			handle_mfw(t_mst *args, int i, char **s);
 void			handle_mfw_i(t_mst *args, int i, char **s, int neg);
@@ -82,12 +87,13 @@ void			handle_plus_i(t_mst *args, char **s, int len, int i);
 void			handle_space_i(t_mst *args, char **s, int i);
 void			handle_pre(t_mst *args, int i, char **s);
 
-/* UTILITIES */
+/*
+** UTILITIES
+*/
 int				count_args(char *fmt);
 int				arg_is_valid(char *fmt, int i);
 void			struct_initializer(t_mst *args, int n);
 void			struct_finalizer(t_mst *args);
 void			i_helper(t_mst *args, int i, char **s);
-
 
 #endif
