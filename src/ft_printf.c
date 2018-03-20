e3a660982bdbb30df0b6ab2091a0f70f2f4380af
Rafael Zarate <rafazrte@gmail.com>
2018-03-19 22:16:48 -0700

    Almost done

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,11 +6,72 @@
 /*   By: rzarate <rzarate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 22:12:29 by rzarate           #+#    #+#             */
-/*   Updated: 2018/03/18 20:16:17 by rzarate          ###   ########.fr       */
+/*   Updated: 2018/03/19 22:16:01 by rzarate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
+
+int	arg_handler(t_mst *args, va_list ap, int i, t_uni *d_type)
+{
+	if (args->id[i] == 'i' || args->id[i] == 'd' || args->id[i] == 'D' ||
+			args->id[i] == 's' || args->id[i] == 'S' || args->id[i] == 'c' ||
+			args->id[i] == 'C')
+		d_type->imt = va_arg(ap, intmax_t);
+	else if (args->id[i] == 'o' || args->id[i] == 'O' || args->id[i] == 'u' ||
+			args->id[i] == 'U' || args->id[i] == 'x' || args->id[i] == 'X' ||
+			args->id[i] == 'C' || args->id[i] == 'p')
+		d_type->uimt = va_arg(ap, uintmax_t);
+	if (args->id[i] == 'c' || args->id[i] == 'C')
+		return (handle_c(args, i, d_type));
+	else if (args->id[i] == 's' || args->id[i] == 'S')
+		return (handle_s(args, i, d_type));
+	else if (args->id[i] == 'i' || args->id[i] == 'd' || args->id[i] == 'D')
+		return (handle_i(args, i, d_type));
+	else if (args->id[i] == 'o')
+		return (handle_o(args, i, d_type));
+	else if (args->id[i] == 'u')
+		return (handle_u(args, i, d_type));
+	else if (args->id[i] == 'x' || args->id[i] == 'X')
+		return (handle_x(args, i, d_type));
+	else if (args->id[i] == '%' || args->id[i] == '%')
+		return (handle_per(args, i));
+	else if (args->id[i] == 'p')
+		return (handle_p(d_type));
+	return (0);
+}
+
+void	arg_scanner(char *fmt, t_mst *args, int n)
+{
+	size_t	i;
+	int		x;
+	int		i2;
+
+	i = -1;
+	i2 = 0;
+	struct_initializer(args, n);
+	while (++i < ft_strlen(fmt) - 1)
+	{
+		if (fmt[i] == '%')
+		{
+			x = i + 1;
+			x += check_flags(fmt, x, args, i2);
+			x += check_mfw(fmt, x, args, i2);
+			if (fmt[x] == '.')
+				x += check_precision(fmt, x, args, i2);
+			x += check_mod(fmt, x, args, i2);
+			args->id[i2] = check_ids(fmt[x]);
+			args->n_chars[i2] = x - i;
+			if (args->id[i2] == -1)
+			{
+				args->n_chars[i2]--;
+				x--;
+			}
+			i += args->n_chars[i2];
+			i2++;
+		}
+	}
+}
 
 static	int	printer(t_mst *args, va_list ap, char *fmt, t_uni *d_type)
 {
