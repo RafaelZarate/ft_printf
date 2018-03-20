ef6008040d15102951776231fad09443e8458fbf
Rafael Zarate <rafazrte@gmail.com>
2018-03-20 11:02:26 -0700

    Adding minor changes

--- src/handlers_3.c
+++ src/handlers_3.c
@@ -6,7 +6,7 @@
 /*   By: rzarate <rzarate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/18 20:45:36 by rzarate           #+#    #+#             */
-/*   Updated: 2018/03/20 10:51:30 by rzarate          ###   ########.fr       */
+/*   Updated: 2018/03/20 11:02:06 by rzarate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,35 +50,28 @@ void	handle_space_i(t_mst *args, char **s, int i)
 void	handle_mfw_i(t_mst *args, int i, char **s, int neg)
 {
 	char	*tmp;
-	int		len;
 	char	*s2;
 	int		i2;
 
 	i2 = -1;
 	tmp = *s;
-	len = args->mfw[i] - (int)ft_strlen(tmp);
-	if (len > 0)
+	if (args->mfw[i] - (int)ft_strlen(tmp) > 0)
 	{
-		s2 = ft_strnew(len);
+		s2 = ft_strnew(args->mfw[i] - (int)ft_strlen(tmp));
 		if (args->zero[i] == 1 && args->minus[i] == 0)
 		{
-			while (++i2 < len)
+			while (++i2 < args->mfw[i] - (int)ft_strlen(tmp))
 				s2[i2] = '0';
-			if (neg == 1)
-			{
-				s2[0] = '-';
-				tmp[0] = '0';
-			}
+			(neg == 1) ? s2[0] = '-' : 0;
+			(neg == 1) ? tmp[0] = '0' : 0;
 			*s = ft_strjoin(s2, tmp);
 		}
 		else
 		{
-			while (++i2 < len)
+			while (++i2 < args->mfw[i] - (int)ft_strlen(tmp))
 				s2[i2] = ' ';
-			if (args->minus[i] == 1)
-				*s = ft_strjoin(tmp, s2);
-			else
-				*s = ft_strjoin(s2, tmp);
+			(args->minus[i] == 1) ? *s = ft_strjoin(tmp, s2) : 0;
+			(args->minus[i] != 1) ? *s = ft_strjoin(s2, tmp) : 0;
 		}
 	}
 }
