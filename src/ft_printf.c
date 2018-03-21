7ac53d397d21fbd135aed89ccb94645c3f840466
Rafael Zarate <rafazrte@gmail.com>
2018-03-21 14:04:42 -0700

    asdf

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rzarate <rzarate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 22:12:29 by rzarate           #+#    #+#             */
-/*   Updated: 2018/03/19 22:28:49 by rzarate          ###   ########.fr       */
+/*   Updated: 2018/03/21 14:03:34 by rzarate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,16 @@
 static	int		arg_handler(t_mst *args, va_list ap, int i, t_uni *d_type)
 {
 	if (args->id[i] == 'i' || args->id[i] == 'd' || args->id[i] == 'D' ||
-			args->id[i] == 's' || args->id[i] == 'S' || args->id[i] == 'c' ||
-			args->id[i] == 'C')
+			args->id[i] == 'c' || args->id[i] == 'C')
 		d_type->imt = va_arg(ap, intmax_t);
 	else if (args->id[i] == 'o' || args->id[i] == 'O' || args->id[i] == 'u' ||
 			args->id[i] == 'U' || args->id[i] == 'x' || args->id[i] == 'X' ||
 			args->id[i] == 'C' || args->id[i] == 'p')
 		d_type->uimt = va_arg(ap, uintmax_t);
+	else if (args->id[i] == 's')
+		d_type->s = va_arg(ap, char *);
+	 else if (args->id[i] == 'S')
+	 	d_type->s = va_arg(ap, wchar_t *)	;
 	if (args->id[i] == 'c' || args->id[i] == 'C')
 		return (handle_c(args, i, d_type));
 	else if (args->id[i] == 's' || args->id[i] == 'S')
