cf581c33491152ffb7495780a00f472c74d64584
Rafael Zarate <rafazrte@gmail.com>
2018-03-21 14:59:38 -0700

    NO LEAKS

--- src/handlers_2.c
+++ src/handlers_2.c
@@ -6,7 +6,7 @@
 /*   By: rzarate <rzarate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/18 20:19:23 by rzarate           #+#    #+#             */
-/*   Updated: 2018/03/21 14:36:22 by rzarate          ###   ########.fr       */
+/*   Updated: 2018/03/21 14:58:11 by rzarate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,23 +26,15 @@ int	handle_s(t_mst *args, int i, t_uni *d_type)
 		else
 			s = ft_strdup(d_type->s);
 	}
-		
-	// if (!d_type->s)
-	// 	{
-	// 		free(s);
-	// 		s = NULL;
-	// 		s = ft_strdup("(null)");
-	// 	}
 	else if ((args->mod[i] == 0 && args->id[i] == 'S') ||
 			(args->mod[i] == 2 && args->id[i] == 's'))
 	{
 		s2 = d_type->wcts;
+		s = ft_strnew(ft_strlen_wc(s2));
 		while (++i2 < (int)ft_strlen_wc(s2))
-			ft_putchar((char)s2[i]);
-		return (ft_strlen_wc(s2));
+			s[i2] = (char)s2[i2];
 	}
 	handle_mfw_s(args, i, &s);
-	
 	return (p_and_f(&s));
 }
 
