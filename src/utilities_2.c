21ee6af77f82b7648f74508ddeb6e1dc1f750709
Rafael Zarate <rafazrte@gmail.com>
2018-03-21 23:30:48 -0700

    FINISHED

--- src/utilities_2.c
+++ src/utilities_2.c
@@ -6,7 +6,7 @@
 /*   By: rzarate <rzarate@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/21 21:16:55 by rzarate           #+#    #+#             */
-/*   Updated: 2018/03/21 23:22:51 by rzarate          ###   ########.fr       */
+/*   Updated: 2018/03/21 23:27:49 by rzarate          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,9 +43,9 @@ void	mfw_oux_helper(t_mst *args, char **s, char **s2, int i)
 		*s = ft_strjoinf(s2, s, 3);
 }
 
-void	check_flags_ca(int *x, char *fmt)
+void	check_flags_ca(size_t *x, char *fmt)
 {
 	while (fmt[*x] && (fmt[*x] == '#' || fmt[*x] == '0' || fmt[*x] == '-' ||
 		fmt[*x] == '+' || fmt[*x] == ' '))
-		*x++;
+		(*x) += 1;
 }
